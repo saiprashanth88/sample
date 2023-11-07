9be581f3a5a3e0cf2f109ffbe0bb7ce4dd745fe9
A Sai Prashanth <95900559+saiprashanth88@users.noreply.github.com>
2023-11-07 19:44:06 +0530

    Update build_files.sh

--- sir/myproject/build_files.sh
+++ sir/myproject/build_files.sh
@@ -1,4 +1,11 @@
 echo "BUILD START"
+# pip install -r requirements.txt
+# python3.9 manage.py collectstatic
+# build_files.sh
 pip install -r requirements.txt
+
+# make migrations
+python3.9 manage.py migrate 
 python3.9 manage.py collectstatic
+
 echo "BUILD END"
